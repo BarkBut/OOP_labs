e065bb537fd7383b3df5234188f990f988b9c663
O. Dobler <61602150+Poltorola@users.noreply.github.com>
2020-03-02 16:32:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -67,31 +67,7 @@ int main() {
 //    int i;
 //    std::cin >> i;
     
-// // #ifndef LAB1_BOOK_H
-// #define LAB1_BOOK_H
 
-// #include <iostream>
-// #include <string.h>
-
-// class printBook : public Book {
-// 	private:
-// 		int printPages;
-		
-// 	public	
-// 	PrintBook(std::string title, std::string author, int pages) : Book(title, author, pages)  // Íîâûé êëàññ ïå÷àòíàÿ Êíèãà íàñëåäóåò ñâîéñòâà íàçâàíèå, àâòîð, ñòðàíèöû
-// 	{
-// 		printPages = pages / 16;
-// 	}
-	
-// 	int getPages()
-// 	{
-// 		return printPages;
-// 	}
-// };    
-    
-    
-    
-    
-    
+  
     return 0;
 }
